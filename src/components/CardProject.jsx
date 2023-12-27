f8898a28e143eae310f82c960764a5ea793e8224
thedarkvader321 <nicomunoz105@gmail.com>
2023-12-27 19:14:33 -0300

    images

---
+++ src/components/CardProject.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+export const CardProject = ({imgUrl,title, text, link}) => {
+    return (
+        <>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={imgUrl} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {link ?(
+                            <a href={link} style={{ textDecoration: 'none', color: 'GrayText' }}></a>
+                        ):(
+                            text
+                        )}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { CarruselFinal } from '../components/CarruselFinal'
-import { Card } from 'react-bootstrap'
+import { CardProject } from '../components/CardProject'
 import { NavBar } from '../components/NavBar'
 import '../style/projects.css'
 export const Projects = () => {
@@ -10,47 +9,21 @@ export const Projects = () => {
       <h1 className='title'>Funciona</h1>
       <section>
         <div className='projects'>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="../assets/icon.jpg" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="../assets/tic.png" />
-            <Card.Body>
-              <Card.Title>Tic-Tac</Card.Title>
-              <Card.Text>
-                <a href="https://silly-mandazi-9b224d.netlify.app/" style={{textDecoration:'none', color:'GrayText'}}>
-                Proyecto realizado en React que recrea el juego tic-tac.
-                </a>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="../assets/icon.jpg" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="../assets/icon.jpg" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
+          <CardProject
+            imgUrl="https://drive.google.com/uc?export=view&id=1S0bHkU4Br2jFW6ATnWvMekCkcRMavYnW"
+            title="Tic-Tac"
+            text="Some quick example text for card 1"
+          />
+          <CardProject
+            imgUrl="https://drive.google.com/uc?export=view&id=1S0bHkU4Br2jFW6ATnWvMekCkcRMavYnW"
+            title="Tic-Tac"
+            text="Some quick example text for card 1"
+          />
+          <CardProject
+            imgUrl="https://drive.google.com/uc?export=view&id=1S0bHkU4Br2jFW6ATnWvMekCkcRMavYnW"
+            title="Tic-Tac"
+            text="Some quick example text for card 1"
+          />
         </div>
       </section>
     </>
